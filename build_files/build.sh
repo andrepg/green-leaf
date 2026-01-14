adfeac5b28c91745ca9803e53da1eac7f977f445
André Paul Grandsire <andrepg@users.noreply.github.com>
2026-01-14 14:49:52 -0300

    move customizations to inside build script

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,4 +12,11 @@ set -ouex pipefail
 # this installs a package from fedora repos
 dnf5 install -y tmux gcc libgcc libgda libgda-sqlite rpmdevtools
 
-sh ./customizations.sh
+# install homebrew
+eval $(curl -fsSL https://raw.githubusercontent.com/andrepg/dotfiles/refs/heads/main/distro-setup/brew.sh)
+
+# install oh-my-bash
+eval $(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)
+
+# install VSCode
+eval $(curl -fsSL https://raw.githubusercontent.com/andrepg/supercow/refs/heads/main/vscode-updater.sh)
